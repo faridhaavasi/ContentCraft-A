abbc6b372970e0565b05aabeeec652c78634ad2b
faridhavasi <105421842+faridhaavasi@users.noreply.github.com>
2025-01-24 21:18:52 +0330

    add users app with custom user model and admin registration

---
+++ code/apps/users/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from appd.users.models import User
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['email', 'first_name', 'last_name', 'is_active', 'is_staff', 'is_superuser', 'is_paid', 'role', 'date_joined']
+    search_fields = ['email', 'first_name', 'last_name']
+    list_filter = ['is_active', 'is_staff', 'is_superuser', 'is_paid', 'role']
+    ordering = ['email']
+
+admin.site.register(User. UserAdmin)
