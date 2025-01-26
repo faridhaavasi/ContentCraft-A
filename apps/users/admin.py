1afd39f8d3a660971ff40f896a2901a09b20245c
faridhavasi <105421842+faridhaavasi@users.noreply.github.com>
2025-01-26 18:38:36 +0330

    fix: correct import path and syntax error in UserAdmin registration

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from appd.users.models import User
+from apps.users.models import User
 
 class UserAdmin(admin.ModelAdmin):
     list_display = ['email', 'first_name', 'last_name', 'is_active', 'is_staff', 'is_superuser', 'is_paid', 'role', 'date_joined']
@@ -7,4 +7,4 @@ class UserAdmin(admin.ModelAdmin):
     list_filter = ['is_active', 'is_staff', 'is_superuser', 'is_paid', 'role']
     ordering = ['email']
 
-admin.site.register(User. UserAdmin)
+admin.site.register(User, UserAdmin)
