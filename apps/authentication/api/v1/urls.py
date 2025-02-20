3457a50dc304b52d5c509ce028ce77742418cfdc
faridhavasi <105421842+faridhaavasi@users.noreply.github.com>
2025-02-20 17:03:38 +0330

    fix: update registration URL path format for consistency

--- apps/authentication/api/v1/urls.py
+++ apps/authentication/api/v1/urls.py
@@ -4,5 +4,5 @@ from apps.authentication.api.v1.views import sigin_signup
 
 app_name = 'authentication-v1'
 urlpatterns = [
-    path('register_stepone/v1', sigin_signup.SteponeRegisterView.as_view(), name='register-stepone'),
+    path('v1/register_stepone/', sigin_signup.SteponeRegisterView.as_view(), name='register-stepone'),
 ]
