e9166db4e7169a2d3bd78e8a5961f17a55eff1f7
faridhavasi <105421842+faridhaavasi@users.noreply.github.com>
2025-02-20 17:19:00 +0330

    refactor: remove unused files and directories for cleanup

--- apps/authentication/api/v1/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from apps.authentication.api.v1.views import sigin_signup
-
-app_name = 'authentication-v1'
-urlpatterns = [
-    path('v1/register_stepone/', sigin_signup.SteponeRegisterView.as_view(), name='register-stepone'),
-]
--- apps/authentication/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path, include
-
-app_name = 'authentication'
-urlpatterns = [
-    path('api/', include('apps.authentication.api.v1.urls', namespace='authentication')),
-]
-
